77aebda5f359e14e32c70806be79536315e4d000
alex-rudyak-dls <aliaksandr.rudziak@digitallifesciences.co.uk>
2015-12-23 17:50:10 +0300

    add root header to centralise access to lib

---
+++ Pod/Classes/Headers/DLSModels.h
@@ -0,0 +1,27 @@
+//
+//  DLSModels.h
+//  Pods
+//
+//  Created by Alex Rudyak on 12/23/15.
+//
+//
+
+#ifndef DLSModels_h
+#define DLSModels_h
+
+#import "DLSAccessTokenWrapper.h"
+#import "DLSApplicationSettingsWrapper.h"
+#import "DLSCategoryWrapper.h"
+#import "DLSDirectoryServiceWrapper.h"
+#import "DLSLocationWrapper.h"
+#import "DLSOrganisationWrapper.h"
+#import "DLSTimetableWrapper.h"
+#import "DLSUserProfileWrapper.h"
+
+#import "DLSAnonymousAppointmentObject.h"
+#import "DLSAppointmentObject.h"
+#import "DLSContactObject.h"
+#import "DLSFeedbackObject.h"
+#import "DLSPracticeObject.h"
+
+#endif /* DLSModels_h */
---
+++ Pod/Classes/Headers/DLSServices.h
@@ -0,0 +1,34 @@
+//
+//  DLSServices.h
+//  Pods
+//
+//  Created by Alex Rudyak on 12/23/15.
+//
+//
+
+#ifndef DLSServices_h
+#define DLSServices_h
+
+#import "DLSTransport.h"
+#import "DLSHttpNetworkTransport.h"
+
+#import "DLSServiceConfiguration.h"
+#import "DLSApplicatinSettingsServiceConfiguration.h"
+#import "DLSAuthenticationServiceConfiguration.h"
+#import "DLSEntityServiceConfiguration.h"
+
+#import "DLSEntityService.h"
+#import "DLSEntityAbstractService.h"
+#import "DLSAuthenticationService.h"
+#import "DLSOrganisationService.h"
+#import "DLSCategoriesService.h"
+#import "DLSApplicationSettingsService.h"
+#import "DLSUserProfileService.h"
+#import "DLSAppointmentsService.h"
+#import "DLSServicesDirectoryService.h"
+#import "DLSPracticesService.h"
+#import "DLSContactsService.h"
+#import "DLSFeedbacksService.h"
+#import "DLSServiceLayer.h"
+
+#endif /* DLSServices_h */
