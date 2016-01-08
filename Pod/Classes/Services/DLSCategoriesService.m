aff04fcbf2e24a0ce8d7cf93e8f938167caebdc8
alex-rudyak-dls <aliaksandr.rudziak@digitallifesciences.co.uk>
2016-01-08 14:30:47 +0300

    cache categories on first sign in

--- Pod/Classes/Services/DLSCategoriesService.m
+++ Pod/Classes/Services/DLSCategoriesService.m
@@ -21,16 +21,6 @@
 - (PMKPromise *)fetchAll
 {
     return [super fetchAll].thenOn(self.fetchQueue, ^() {
-        [self updateMediumPaths];
-        [self.transport setAuthorizationHeader:[self.authService.token authenticationHeaderValue]];
-        return [self.transport fetchAllWithParams:nil];
-    }).thenOn(self.fetchQueue, ^(id response) {
-        NSArray<DLSCategoryObject *> *categories = [EKMapper arrayOfObjectsFromExternalRepresentation:response withMapping:[DLSCategoryObject objectMapping]];
-        if (!categories.count) {
-            [self _successWithResponse:@[] completion:nil];
-            return [PMKPromise promiseWithValue:@[]];
-        }
-
         NSError *error;
         RLMRealm *realm = [RLMRealm realmWithConfiguration:self.serviceConfiguration.realmConfiguration error:&error];
         if (error) {
@@ -38,14 +28,40 @@
             @throw error;
         }
 
-        [realm beginWriteTransaction];
-        [realm addOrUpdateObjectsFromArray:categories];
-        [realm commitWriteTransaction];
+        RLMResults *categories = [DLSCategoryObject allObjectsInRealm:realm];
+        NSMutableArray<DLSCategoryWrapper *> *categoriesWrappers = [NSMutableArray arrayWithCapacity:categories.count];
+        if (categories.count) {
+            for (DLSCategoryObject *category in categories) {
+                [categoriesWrappers addObject:[DLSCategoryWrapper categoryWithObject:category]];
+            }
+            return [PMKPromise promiseWithValue:[NSArray arrayWithArray:categoriesWrappers]];
+        }
 
-        NSArray<DLSCategoryWrapper *> *wrappers = [Underscore array](categories).map(^(DLSCategoryObject *category) {
-            return [DLSCategoryWrapper categoryWithObject:category];
-        }).unwrap;
-        return [PMKPromise promiseWithValue:wrappers];
+        [self updateMediumPaths];
+        [self.transport setAuthorizationHeader:[self.authService.token authenticationHeaderValue]];
+        return [self.transport fetchAllWithParams:nil].thenOn(self.fetchQueue, ^(id response) {
+            NSArray<DLSCategoryObject *> *categories = [EKMapper arrayOfObjectsFromExternalRepresentation:response withMapping:[DLSCategoryObject objectMapping]];
+            if (!categories.count) {
+                [self _successWithResponse:@[] completion:nil];
+                return [PMKPromise promiseWithValue:@[]];
+            }
+
+            NSError *error;
+            RLMRealm *realm = [RLMRealm realmWithConfiguration:self.serviceConfiguration.realmConfiguration error:&error];
+            if (error) {
+                [self _failWithError:error inMethod:_cmd completion:nil];
+                @throw error;
+            }
+
+            [realm beginWriteTransaction];
+            [realm addOrUpdateObjectsFromArray:categories];
+            [realm commitWriteTransaction];
+
+            NSArray<DLSCategoryWrapper *> *wrappers = [Underscore array](categories).map(^(DLSCategoryObject *category) {
+                return [DLSCategoryWrapper categoryWithObject:category];
+            }).unwrap;
+            return [PMKPromise promiseWithValue:wrappers];
+        });
     }).thenOn(self.responseQueue, ^(NSArray *categories) {
         [self _successWithResponse:categories completion:nil];
         return [PMKPromise promiseWithValue:categories];
